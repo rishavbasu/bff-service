55688d5a87bd19cc634a94d77dad58c2e1e887ac
RISHAV BASU <908263@TCS.com>
2020-10-07 18:46:19 +0530

    initial commit

---
+++ src/main/java/org/rishav/bffservice/BFFService.java
@@ -0,0 +1,20 @@
+package org.rishav.bffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class BFFService {
+    public static void main(String[] args) {
+        SpringApplication.run(BFFService.class, args);
+    }
+
+    @RequestMapping(value = "/bff-service/hello")
+    String hello() {
+        return new RestTemplate().getForObject("http://beservice/products/hello", String.class);
+    }
+}
